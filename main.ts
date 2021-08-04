2e7be5bd79272150f328323b9185ca6ef979183c
Joost Plattel <jsplattel@gmail.com>
2021-08-04 23:03:48 +0200

    added templating to window title

--- main.ts
+++ main.ts
@@ -1,15 +1,26 @@
-import { Plugin, TFile } from 'obsidian';
+import { App, Plugin, PluginSettingTab, Setting, TFile } from 'obsidian';
 
 export default class ActiveNoteTitlePlugin extends Plugin {
 	// Get the window title
 	baseTitle = document.title;
 
 	async onload() {
+		// Show the plugin is loading for developers
 		console.log('loading ActiveNoteTitlePlugin plugin');
+
 		// When opening, renaming or deleting a file, update the window title
 		this.registerEvent(this.app.workspace.on('file-open', this.handleOpen));
 		this.registerEvent(this.app.vault.on('rename', this.handleRename));
 		this.registerEvent(this.app.vault.on('delete', this.handleDelete));
+
+		// Load the settings
+		await this.loadSettings(); 
+
+		// Add the settings tab
+		this.addSettingTab(new ActiveNoteTitlePluginSettingsTab(this.app, this));
+
+		// Finally call the refresh title now..
+		this.refreshTitle()
 	}
 
 	// Restore original title on unload.
@@ -17,20 +28,114 @@ export default class ActiveNoteTitlePlugin extends Plugin {
 		document.title = this.baseTitle;
 	}
 
+	// The main method that is responsible for updating the title
+	refreshTitle(file?: TFile) {
+		// For the template, the vault and workspace are always available
+		let template = {
+			'vault': this.app.vault.getName(),
+			'workspace': this.app.internalPlugins.plugins.workspaces.instance.activeWorkspace // Defaults to: '' if not enabled
+		}
+
+		if (file) {
+			// If a file is open, the filename, path and frontmatter is added 
+			let frontmatter = this.app.metadataCache.getFileCache(file).frontmatter
+			template = {
+				'filename': file.name,
+				'filepath': file.path,
+				...frontmatter,
+				...template
+			}
+			document.title = this.templateTitle(template, this.settings.titleTemplate)
+		} else {
+			document.title = this.templateTitle(template, this.settings.titleTemplateEmpty)
+		}
+	}
+
+	templateTitle(template, title: String): String {
+		Object.keys(template).forEach(key => {
+			title = title.replace(`{${key}}`, template[key])
+		})
+		console.log(title)
+		return title
+	}
+	
 	private readonly handleRename = async (file: TFile): Promise<void> => {
-		document.title = this.baseTitle + ' - ' + file.path;
+		if (file instanceof TFile && file === this.app.workspace.getActiveFile()) {
+			this.refreshTitle(file);
+		}
 	};
 
 	private readonly handleDelete = async (file: TFile): Promise<void> => {
-		document.title = this.baseTitle;
+		if (this.app.workspace.getActiveFile() === null || this.app.workspace.getActiveFile() === undefined) {
+			this.refreshTitle();
+		}
 	};
 
 	private readonly handleOpen = async (file: TFile): Promise<void> => {
 		if (file instanceof TFile) {
-			document.title = this.baseTitle + ' - ' + file.path;
+			this.refreshTitle(file);
 		} else {
-			document.title = this.baseTitle;
+			this.refreshTitle();
 		}
 	};
 
+	async loadSettings() {
+		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	}
+
+	async saveSettings() {
+		await this.saveData(this.settings);
+	}
+
+}
+
+
+interface ActiveNoteTitlePluginSettings {
+	titleTemplate: string,
+	titleTemplateEmpty: string,
+}
+
+const DEFAULT_SETTINGS: ActiveNoteTitlePluginSettings = { 
+	titleTemplate: "Obsidian - {vault} - {filename}",
+	titleTemplateEmpty: "Obsidian - {vault} - {filename}",
+}
+
+class ActiveNoteTitlePluginSettingsTab extends PluginSettingTab {
+
+	plugin: ActiveNoteTitlePlugin;
+
+	constructor(app: App, plugin: ActiveNoteTitlePlugin) {
+		super(app, plugin);
+		this.plugin = plugin;
+	}
+
+	display(): void {
+		let { containerEl } = this;
+		
+		containerEl.empty();
+		containerEl.createEl('h2', {text: 'Window title templates'});
+		containerEl.createEl('p', {text: 'These two templates override the window title of the Obsidian window. This is useful for example when you use tracking software that works with window titles. '});
+
+		new Setting(containerEl)
+			.setName('Default template for window title (applicable when no file is open)')
+			.setDesc('You can use the following placeholders: {vault}, {workspace}')
+			.addText(text => text.setPlaceholder("Obsidian - {vault}")
+				.setValue(this.plugin.settings.titleTemplateEmpty || "Obsidian - {vault}")
+				.onChange((value) => {
+					this.plugin.settings.titleTemplateEmpty = value;
+					this.plugin.saveData(this.plugin.settings);
+					this.plugin.refreshTitle();
+				}));
+
+		new Setting(containerEl)
+			.setName('Template for window title when a file is opened or changed')
+			.setDesc('You can use the following placeholders: {vault}, {workspace}, {filename}, {filepath} and {<any_frontmatter_key>}')
+			.addText(text => text.setPlaceholder("Obsidian - {vault} - {filename}")
+			.setValue(this.plugin.settings.titleTemplate || "Obsidian - {vault} - {filename}")
+			.onChange((value) => {
+				this.plugin.settings.titleTemplate = value;
+				this.plugin.saveData(this.plugin.settings);
+				this.plugin.refreshTitle();
+			}));
+	}
 }
